e73f5434970e26fc8b2a324ffb1cfca5448d0039
Satyajeet27 <satyajeet.hitk@gmail.com>
2024-06-12 12:54:49 +0530

    restructre router dom

--- src/App.js
+++ src/App.js
@@ -2,50 +2,19 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import RestaurantMenu from "./components/RestaurantMenu";
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Contact from "./components/Contact";
 import ReactDOM from 'react-dom/client';
 import Resturant from "./components/Resturant";
 
 
 
-const App = () => {
+export const App = () => {
   return (
     <div className="App">
-      <Header/>
-      <Outlet/>
+      <Header />
+      <Outlet />
     </div>
   );
 };
 
-const  appRouter = createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>,
-    children: [
-      {
-        path:"/",
-        element:<Body/>
-      },
-      {
-        path:"/About",
-        element:<About/>
-      },
-      {
-        path:"/Contact",
-        element:<Contact />
-      },
-      {
-        path:"/restaurants/:resId",
-        element:<RestaurantMenu/>
-      },
-    
-    ],
-  },
-  
-]);
-
-const root=ReactDOM.createRoot(document.getElementById('root'))
-//root.render(<App/>)
-root.render(<RouterProvider router={appRouter}/>)
-//export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RestaurantMenu from './components/RestaurantMenu';
+import Contact from './components/Contact';
+import About from './components/About';
+import Body from './components/Body';
+
 
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +20,35 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 //reportWebVitals();
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/About",
+        element: <About />
+      },
+      {
+        path: "/Contact",
+        element: <Contact />
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />
+      },
+
+    ],
+  },
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+//root.render(<App/>)
+root.render(<React.StrictMode><RouterProvider router={appRouter} /></React.StrictMode>)
+//export default App;
